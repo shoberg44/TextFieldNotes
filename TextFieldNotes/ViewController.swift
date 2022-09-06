399f76523903f5b0afa95b71faf52a163f11fb9e
shoberg44 <steven.hoberg23@d155.org>
2022-09-06 07:35:55 -0500

    switched sum stuff around (Look at constant )

--- TextFieldNotes/ViewController.swift
+++ TextFieldNotes/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var submitOutlet: UITextField!
+    @IBOutlet weak var submitOutlet2: UITextField!
+    @IBOutlet weak var nameOut: UILabel!
+    
+    
+    @IBOutlet weak var nameIn: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+       // submitButton.layer.cornerRadius = 5
+       // submitButton.clipToBounds = true
     }
+    @IBAction func nameSubmit(_ sender: UIButton) {
+        let name = nameIn.text
+        nameOut.text = name
+        }
 
+    @IBAction func submitButton(_ sender: UIButton) {
+        let num1 = Int(submitOutlet.text!)!
+        let num2 = Int(submitOutlet2.text!)!
+        let sum = num1 + num2
+        labelOutlet.text = "Sum is \(sum)"
+    }
+    
+    @IBAction func nameEnter(_ sender: UIButton) {
+    }
+    
+    
+      
+    
 
 }
 
