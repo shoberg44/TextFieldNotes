fb88c04f71aa72dade58cbe24617379f2602e8c3
shoberg44 <steven.hoberg23@d155.org>
2022-09-09 07:48:13 -0500

    Initial Commit

--- TextFieldNotes/ViewController.swift
+++ TextFieldNotes/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var labelOutlet: UILabel!
     @IBOutlet weak var submitOutlet: UITextField!
+    
     @IBOutlet weak var submitOutlet2: UITextField!
     @IBOutlet weak var nameOut: UILabel!
     
@@ -24,20 +25,38 @@ class ViewController: UIViewController {
        // submitButton.clipToBounds = true
     }
     @IBAction func nameSubmit(_ sender: UIButton) {
-        let name = nameIn.text
-        nameOut.text = name
+        let name = nameIn.text!
+        nameOut.text = "Hello, " + name
         }
 
     @IBAction func submitButton(_ sender: UIButton) {
+        let num1 = Int(submitOutlet.text!)!
+                let num2 = Int(submitOutlet2.text!)!
+                let prod = num1 * num2
+                labelOutlet.text = "Product is \(prod)"
+    }
+    @IBAction func submitButton2(_ sender: UIButton) {
         let num1 = Int(submitOutlet.text!)!
         let num2 = Int(submitOutlet2.text!)!
         let sum = num1 + num2
         labelOutlet.text = "Sum is \(sum)"
     }
     
-    @IBAction func nameEnter(_ sender: UIButton) {
+    @IBAction func hypeOut(_ sender: UIButton) {
+        var num1 = Double(submitOutlet.text!)!
+        var num2 = Double(submitOutlet2.text!)!
+        num2 = pow(num2, 2)
+        num1 = pow(num1, 2)
+        labelOutlet.text = "Hypotenuse is  \(sqrt(num1 + num2))"
     }
-    
+    @IBAction func stepper(_ sender: UIStepper) {
+    if labelOutlet.text! == "" {
+        labelOutlet.text = "\(Double(labelOutlet.text!)! + 1)"
+    }
+    else{
+        labelOutlet.text = "1"
+    }
+    }
     
       
     
